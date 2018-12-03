220c25a007fe9a670d46df9fd17f4c4e5fd632ae
Benjamin Mintz <bmintz@protonmail.com>
2018-12-03 02:39:04 +0000

    generator.py: add a remark about implementing generators without yield

--- generator.py
+++ generator.py
@@ -3,6 +3,8 @@
 
 # Douglas Crockford's idea for making generators
 # basically "why do you need a `yield` keyword when you can just maintain some state"
+# in my view, a class would be a better way to do this, and indeed, in python,
+# that's how Iterators are defined.
 def iter(list):
 	i = 0
 	def gen():
