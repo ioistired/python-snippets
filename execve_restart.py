efe4f6c4ab7570a8ce5644c3a811d6b18e55a277
bmintz <bmintz@protonmail.com>
2018-12-11 18:35:59 +0000

    update execve_restart

--- execve_restart.py
+++ execve_restart.py
@@ -1,6 +1,11 @@
+#!/usr/bin/env python3
+# encoding: utf-8
+
 import sys, os
 
 print('y')
 
-if sys.argv[1] == 'loop':
+if len(sys.argv) > 1 and sys.argv[1] == 'loop':
 	os.execve(sys.executable, ['python'] + sys.argv, os.environ)
+
+# run this script as "./execve_restart.py loop" for the slowest "yes(1)" you will ever see
