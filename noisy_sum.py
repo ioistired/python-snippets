dcd16191fec8e0b056297ec49ebedf34093dea9d
Benjamin Mintz <bmintz@protonmail.com>
2018-12-14 22:50:36 +0000

    document noisy_sum a bit more

--- noisy_sum.py
+++ noisy_sum.py
@@ -2,6 +2,8 @@
 # encoding: utf-8
 
 # goal is to get sum() to print what it's summing
+# i want to see if it's lazy or gathers through the whole iterable first
+# especially with sys.stdin
 
 nums = range(10)
 
@@ -10,6 +12,8 @@ class NoisyInt(int):
 		print(self, '+', other)
 		return super().__add__(other)
 
+	# radd is necessary since sum is foldl (+) 0
+	# where 0 is a regular int, not a NoisyInt
 	__radd__ = __add__
 
 if __name__ == '__main__':
