a70cab43a0f840073e5034af6ffd1a3a0c7129eb
Benjamin Mintz <bmintz@protonmail.com>
2018-12-21 08:11:37 +0000

    add async init thanks to @MysterialPy

---
+++ async_init.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+# encoding: utf-8
+
+import asyncio
+import functools
+import random
+
+def async_init(cls):
+	__new__ = cls.__new__
+
+	@functools.wraps(__new__)
+	async def new(cls, *args, **kwargs):
+		result = __new__(cls, *args, **kwargs)
+
+		await result.__init__(*args, **kwargs)
+		return result
+
+	cls.__new__ = new
+	return cls
+
+@async_init
+class DatabaseConnection:
+	established = False
+
+	async def __init__(self):
+		# do some network IO to establish the connection
+		await asyncio.sleep(random.random())
+		self.established = True
+
+async def test():
+	conn = await DatabaseConnection()
+	assert conn.established
+
+if __name__ == '__main__':
+	asyncio.get_event_loop().run_until_complete(test())
