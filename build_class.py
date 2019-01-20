5066bfa60806e024270e412b1247b646de11b201
bmintz <bmintz@protonmail.com>
2019-01-20 02:36:42 +0000

    build_class: add comments

--- build_class.py
+++ build_class.py
@@ -3,6 +3,8 @@
 import builtins
 import dis
 
+# capture the generated internal class function whenever a new class is built
+
 old_build_class = __build_class__
 class_funcs = []
 
@@ -12,11 +14,33 @@ def __build_class__(func, *args, **kwargs):
 
 builtins.__build_class__ = __build_class__
 
-class Foo: x = 1
+# let's build a class to see how it works
 
-foo = class_funcs[0]
+class Foo:
+	x = 1
+
+foo = class_funcs[-1]  # the function corresponding to Foo
 print(foo)
 dis.dis(foo)
 
-foo()
+# the bytecode:
+#  0 LOAD_NAME        0 (__name__)
+#  2 STORE_NAME       1 (__module__)
+#  4 LOAD_CONST       0 ('Foo')
+#  6 STORE_NAME       2 (__qualname__)
+#
+#  8 LOAD_CONST       1 (1)
+# 10 STORE_NAME       3 (x)
+# 12 LOAD_CONST       2 (None)
+# 14 RETURN_VALUE
+
+# hm, these are global assignments.
+
+def closure():
+	def inner():
+		foo()
+	inner()
+closure()
+
+assert 'x' in globals()
 assert x == 1
