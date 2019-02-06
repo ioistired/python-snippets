855f0824caa76417129a209857587e2143da1e6f
bmintz <bmintz@protonmail.com>
2019-02-06 18:25:25 +0000

    implement anonymously recursive defaultdict

--- anonymously_recursive_defaultdict.py
+++ anonymously_recursive_defaultdict.py
@@ -15,3 +15,16 @@ d[1][2] = 3
 assert d[1] == {2: 3}
 
 # but can we do it without named recursion?
+
+(lambda f: f(f))(lambda f: lambda: defaultdict(f))
+# doesn't work (missing required positional argument f)
+
+Z = lambda f: (lambda x: x(x))(lambda y: f(lambda v: y(y)(v)))
+# we use a modified version of Z that works on a zero argument function
+Z = lambda f: (lambda x: x(x))(lambda y: f(lambda: y(y)()))
+
+a = Z(lambda f: lambda: defaultdict(f))
+d = a()
+assert d[1][2][3] == {}
+d[1][2] = 3
+assert d[1] == {2: 3}
