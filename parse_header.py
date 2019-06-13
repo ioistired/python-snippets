cf1c572a56fcce05d8397a8e7e82268c6e1870ba
Benjamin Mintz <bmintz@protonmail.com>
2019-06-13 19:00:11 +0000

    add 3.10 replacement for cgi.parse_header

---
+++ parse_header.py
@@ -0,0 +1,11 @@
+"""
+python 3.10 replacement for cgi.parse_header
+"""
+
+from email.message import EmailMessage
+
+def parse_header(h):
+	m = EmailMessage()
+	m['content-type'] = h
+	l = m.get_params()
+	return ('='.join(l[0]), dict(l[1:]))
