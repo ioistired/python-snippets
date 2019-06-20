d9b13707a254312374d26f7419596cb359a9ed16
Benjamin Mintz <bmintz@protonmail.com>
2019-06-20 10:08:20 +0000

    add very difficult deduplication problem

---
+++ tricky_dedupe_problem.py
@@ -0,0 +1,87 @@
+#!/usr/bin/env python3
+
+import asyncio
+import contextlib
+import inspect
+
+@contextlib.asynccontextmanager
+async def pool():
+	try:
+		yield "connection"
+	finally:
+		print("the connection is now closed")
+
+CONNECTION = None
+
+def set_connection(connection):
+	global CONNECTION
+	CONNECTION = connection
+
+def get_connection():
+	return CONNECTION
+
+def wrap(func):
+	if inspect.isasyncgenfunction(func):
+		async def inner(*args, **kwargs):
+			try:
+				1/0
+			except ZeroDivisionError:
+				async with pool() as conn:
+					set_connection(conn)
+					async for x in func(*args, **kwargs):
+						yield x
+			else:
+				async for x in func(*args, **kwargs):
+					yield x
+	else:
+		async def inner(*args, **kwargs):
+			try:
+				1/0
+			except ZeroDivisionError:
+				async with pool() as conn:
+					set_connection(conn)
+					return await func(*args, **kwargs)
+			else:
+				return await func(*args, **kwargs)
+
+	return inner
+
+awaitable = lambda x: asyncio.sleep(0, x)
+
+class WasteOfSpace:
+	def __init__(self, *args):
+		self.args = args
+		self.index = -1
+
+	def __aiter__(self):
+		return self
+
+	def __anext__(self):
+		self.index += 1
+		if self.index >= len(self.args):
+			raise StopAsyncIteration
+		return self.args[self.index]
+
+@wrap
+async def agen(a, b):
+	print("agen")
+	print(' ', a, b)
+	print(' ', get_connection())
+	async for i in WasteOfSpace(awaitable("a"), awaitable("b")):
+		yield i
+
+@wrap
+async def acor(a, b):
+	print("acor")
+	print(" ", a, b)
+	print(" ", get_connection())
+	return await awaitable("blah")
+
+async def main():
+	async for x in agen(1, 2):
+		pass
+
+	await acor(1, 2)
+
+if __name__ == '__main__':
+	asyncio.run(main())
