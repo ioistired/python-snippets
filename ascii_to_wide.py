48f33b2aa7aa18f31ca0c9ac4b09328ed7ea7aa9
Benjamin Mintz <bmintz@protonmail.com>
2019-09-11 00:08:50 +0000

    fix up ascii to wide

--- ascii_to_wide.py
+++ ascii_to_wide.py
@@ -1,14 +1,13 @@
-table = {chr(i): chr(i + 0xFEE0) for i in range(33, 127)}
+#!/usr/bin/env python3
+
+table = {i: chr(i + 0xFEE0) for i in range(33, 127)}
+table[ord(' ')] = '\N{IDEOGRAPHIC SPACE}'
 
 def ascii_to_fullwidth(text):
 	return text.translate(table)
 
-print(ascii_to_fullwidth('h'))
-assert ascii_to_fullwidth('h') == 'ｈ'
-assert table['h'] == 'ｈ'
+def main():
+	assert ascii_to_fullwidth('h') == 'ｈ'
 
-# why not test all inputs while we're at it?
-for i in range(33, 127):
-	input = chr(i)
-	assert ascii_to_fullwidth(chr(i)) == input # passes always
-	assert table[input] != input # passes always
+if __name__ == '__main__':
+	main()
