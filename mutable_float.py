c546b9a973876e3385a842fd21e6867a1bd2ea6f
Io Mintz <io@mintz.cc>
2019-10-18 19:44:46 +0000

    MutableFloat: add in-place methods

--- mutable_float.py
+++ mutable_float.py
@@ -1,4 +1,5 @@
 import ctypes
+import operator
 import sys
 
 SIZEOF_PYOBJECT = sys.getsizeof(object())
@@ -7,3 +8,11 @@ class MutableFloat(float):
 	def set(self, val):
 		ob_fval = ctypes.c_double.from_address(id(self) + SIZEOF_PYOBJECT)
 		ob_fval.value = val
+
+	for op in operator.add, operator.sub, operator.mul, operator.truediv, operator.floordiv, operator.pow:
+		def meth(self, other, *, _op=op):
+			self.set(_op(self, other))
+			return self  # why tho
+		vars()[f'__i{op.__name__}__'] = meth
+
+	del op, meth
