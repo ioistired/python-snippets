b4d0010fa5e20ffc3ce31b0526190bcbbeb27af8
Io <io@mintz.cc>
2019-12-12 20:14:26 +0000

    add tool to merge two signal safety numbers

---
+++ signal_safety_number_merge.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+
+import itertools
+import functools
+
+def chunk(n, seq):
+	"""split seq into n equal-ish sized parts. The result, when flattened, shall be equal to seq."""
+	chunk_len = len(seq) // n + 1
+	for chunk_start in range(0, len(seq), chunk_len):
+		yield seq[chunk_start : chunk_start + chunk_len]
+
+halve = functools.partial(chunk, 2)
+
+def merge_safety_numbers(a, b):
+	"""Given your safety numbers with two people, return the safety numbers between those two people."""
+	a_fingerprints = set(map(str.strip, halve(a)))
+	b_fingerprints = set(map(str.strip, halve(b)))
+
+	return ' '.join(sorted(a_fingerprints.symmetric_difference(b_fingerprints)))
+
+def test_merge_safety_numbers():
+	assert merge_safety_numbers('823 456 987 654', '767 530 823 456') == '767 530 987 654'
+
+def main():
+	import sys
+	args = sys.argv[1:]
+	print(merge_safety_numbers(*args))
+
+if __name__ == '__main__':
+	main()
