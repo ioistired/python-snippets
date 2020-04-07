8cecda41cbf287099514290f1b99316649b55ba4
io mintz <io@mintz.cc>
2020-04-07 01:40:51 +0000

    add deco which dedents all constant strings

---
+++ dedent_decorator.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+
+import textwrap
+
+def dedent_all(f):
+	consts = list(f.__code__.co_consts)
+	for i, const in enumerate(consts):
+		if type(const) is str:
+			consts[i] = textwrap.dedent(const)
+	f.__code__ = f.__code__.replace(co_consts=tuple(consts))
+	return f
+
+@dedent_all
+def foo():
+	return """
+	hi
+	there
+	"""
+
+if __name__ == '__main__':
+	assert foo() == '\nhi\nthere\n'
