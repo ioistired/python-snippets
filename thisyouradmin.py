f6876a6f02027963ef529bc2c17cafe2da5a6f04
io <gie9ohbeixah@paperboats.net>
2021-05-12 09:58:35 +0000

    make thisyouradmin.py actually useful, fix transparency bug

--- thisyouradmin.py
+++ thisyouradmin.py
@@ -12,16 +12,18 @@ with (
 	wand.image.Image(blob=sys.stdin.buffer.read()) as to_insert,
 ):
 	to_insert.transform(resize=f'621x{to_insert.width}')
-	out = wand.image.Image(width=this_your_admin.width, height=this_your_admin.height + to_insert.height + you_clowns.height)
-	out.background_color = wand.color.Color('#191b22')
-#	post_background = wand.image.Image(width=out.width - 10, height=to_insert.height)
-#	post_background.background_color = wand.color.Color('#313543')
+	out = wand.image.Image(
+		width=this_your_admin.width,
+		height=this_your_admin.height + to_insert.height + you_clowns.height,
+	)
 	out.composite(this_your_admin, 0, 0)
-#	out.composite(post_background, 5, 130)
 	with wand.drawing.Drawing() as post_background:
+		post_background.fill_color = wand.color.Color('#191b22')
+		post_background.rectangle(left=0, top=130, right=out.width, bottom=130+to_insert.height)
 		post_background.fill_color = wand.color.Color('#313543')
 		post_background.rectangle(left=5, top=130, right=out.width-7, bottom=130+to_insert.height)
 		post_background(out)
 	out.composite(to_insert, 28, 130)
 	out.composite(you_clowns, 0, 130 + to_insert.height)
-	display(out)
+	with out.convert('png') as out:
+		out.save(file=sys.stdout.buffer)
